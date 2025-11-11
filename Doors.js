f33d5d177504428ebb3ceeb74e41eb2cd6077943
Xason <136676124+XasonYTB@users.noreply.github.com>
2025-11-11 12:02:35 +0100

    Add Settings category with destroy script option and other settings

---
+++ Doors.js
@@ -0,0 +1,14 @@
+// Existing code up to the closing bracket
+
+// New Settings category beginning
+Settings: {
+    destroy_script: false,
+    notification_settings: {
+        email_notifications: false,
+        push_notifications: false
+    },
+    reset_all_toggles: function() {
+        // function to reset all toggles
+    }
+},
+// Closing bracket for the script
